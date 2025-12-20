a3e9e319dc087ac667b5e357e7af616da564a5ef
rajsahab986-oss <rajsahab986@gmail.com>
2025-12-21 00:19:51 +0530

    Update API credentials and environment variables

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", "28935416"))
-API_HASH = getenv("API_HASH", "e18c05697d95edfe39d8957f6e110308")
+API_ID = int(getenv("API_ID", "16"))
+API_HASH = getenv("API_HASH", "0308")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "7170328188").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", 88").split()))
 MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "-1002507994479")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002507994479"))
+LOG_GROUP = getenv("LOG_GROUP", "-1")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-100))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "5000"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
