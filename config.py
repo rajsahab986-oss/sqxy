7524d7281e993b93209a1c2fa86f2fc7cdb64f2b
rajsahab986-oss <rajsahab986@gmail.com>
2026-06-02 09:46:00 +0530

    Update API credentials and owner ID in config.py

--- config.py
+++ config.py
@@ -13,13 +13,13 @@ YTUB_COOKIES = """
 # write here yt cookies
 """
 
-API_ID = int(getenv("API_ID", "16"))
-API_HASH = getenv("API_HASH", "0308")
+API_ID = int(getenv("API_ID", "29136894"))
+API_HASH = getenv("API_HASH", "88f3d07b70de48ac1fc13866b0c9e562")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", 88").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "7170328188").split()))
 MONGO_DB = getenv("MONGO_DB", "")
-LOG_GROUP = getenv("LOG_GROUP", "-1")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-100))
+LOG_GROUP = getenv("LOG_GROUP", "-1002507994479")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002507994479))
 FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "5000"))
 WEBSITE_URL = getenv("WEBSITE_URL", "upshrink.com")
